f301b90e5497936432705fb2dc13335a933b760e
Neil Gardner <neilgardner1963@gmail.com>
2016-03-05 21:59:52 +0000

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on("connect", function(e){
 	console.log("connected to socket.io server");
 });
+
+
+socket.on("message", function(message){
+	console.log(message.text);
+});
